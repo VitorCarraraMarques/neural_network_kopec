b09ebe882de6f593b6e6c2c10d02b0c4c81f0e2c
Vítor Carrara <vitorcarrara1998@gmail.com>
2022-07-27 11:25:32 -0300

    Add comments explaining what the neuron class does.

--- neuron.py
+++ neuron.py
@@ -13,3 +13,10 @@ class Neuron:
     def output(self, inputs):
         self.output_cache = dot_product(inputs, self.weights)
         return self.activation_function(self.output_cache)
+
+""" 
+Um neurônio nessa rede neural é relativamente simples. Ele é iniciado com uma série de parâmetros, os quais podem 
+ser alterados a qualquer momento ao longo do funcionamento da rede neural. O seu propósito é um únio: receber um 
+sinal (input) para em seguida retorná-lo ativado. Isto é, calcula o produto escalar do input com os pesos (que foram 
+iniciados em init) e ativa esse resultado através da função sigmoid (a nossa função de ativação).
+"""
